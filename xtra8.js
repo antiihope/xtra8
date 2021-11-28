8d883cf065f25b68de69274d5c56bd2b5623cc9e
hazem223 <65074998+hazem223@users.noreply.github.com>
2021-11-28 03:04:06 +0200

    Update xtra8.js

--- xtra8.js
+++ xtra8.js
@@ -1,39 +1,49 @@
-import * as NaminAnythingBlah from './babel.min.js';
-
 const GenID = () => {
   let r = (Math.random() + 1).toString(36).substring(7);
   return r;
 };
-window.print = console.log;
 
+window.print = console.log;
+let els = {};
 var str = JSON.stringify;
 
 function CheckType(type) {
   if (window.useEffect == undefined) {
     window.useEffect = {};
   }
-  if (window.els == undefined) {
-    window.els = {};
+  if (els == undefined) {
+    els = {};
   }
-  if (window.els[type] !== undefined) {
-    window.els[type] = window.els[type] + 1;
+  if (els[type] !== undefined) {
+    els[type] = els[type] + 1;
   } else {
-    window.els[type] = 1;
+    els[type] = 0;
   }
 
-  return window.els[type];
+  return els[type];
+  // try {
+  //   z = document.querySelector('#parent');
+  //   z = z.firstElementChild;
+  //   var e = z.querySelectorAll(type).length + 1;
+  //   return e + z.childElementCount;
+  // } catch (error) {
+  //   return 0;
+  // }
 }
 function SET_VALUES(values, type, children) {
+  var PAGE = ThisPageName();
+  var COUNT = CheckType(type);
+
   if (values == null) values = {};
   if (values.hasOwnProperty('if')) {
     if (!values.if || values.if == undefined) {
       return 'stop';
     }
   }
-  var PAGE = ThisPageName();
-  var COUNT = CheckType(type);
   var id;
+
   id = PAGE + type + COUNT;
+
   // assign id if not already there  , for easier  Callback
 
   if (values == null) {
@@ -41,9 +51,11 @@ function SET_VALUES(values, type, children) {
 
     values.id = id;
   }
+
   if (values.id == undefined) values.id = id;
   var id = values.id;
-  if (id.length == 0) id = name;
+
+  if (id.length == 0) id = id;
 
   // look for useeffect and call it or ignore it
   var Checkin = values.useEffect || type.useEffect || false;
@@ -64,14 +76,17 @@ var React = {
       print(type, values);
     }
 
-    var props = SET_VALUES(values, type, children);
+    var props = SET_VALUES(values, type, children.length);
+
     if (props == 'stop') {
       return '';
     }
+
     if (type !== undefined) {
       if (typeof type == 'function') {
-        var props = SET_VALUES(values, type);
+        var props = SET_VALUES(values, type, children.length);
         props.id = type.name;
+
         props.children = children;
         type.props = props;
         return type(props);
@@ -112,24 +127,34 @@ window.React = React;
 window.el = (x) => {
   return document.getElementById(x);
 };
+Element.prototype.insertChildAtIndex = function (child, index) {
+  if (!index) index = 0;
+  if (index >= this.children.length) {
+    this.appendChild(child);
+  } else {
+    this.insertBefore(child, this.children[index]);
+  }
+};
 
 function UPDATE_DOM(newdom = false) {
-  while (window.els !== undefined) {
-    delete window.els;
+  if (!window.Loaded) {
+    print('not yet loaded');
+    window.finished = true;
+    window.Loaded = true;
+    return;
   }
 
-  // Reset elements count > elements count was made for id , head back to set values uprthere
   window['focused'] = document.activeElement.id;
   var external;
   external = true;
   if (newdom == false) {
     external = false;
 
-    delete window.els;
+    els = {};
+
     var newdom = window.vdom();
   } else {
     external = newdom;
-
     newdom = external();
   }
   var root = el('parent');
@@ -146,41 +171,242 @@ function UPDATE_DOM(newdom = false) {
 
     //  STOP JUDGIN ME .. .I WAS DRUNK ↓↓
     function tryes(repeat = false) {
+      window.fullscreen = document.fullscreenElement;
+
       window.finished = false;
       try {
-        function childtoarray(x) {
-          // pass node element , turn it into object of its id and inner html
-          var z = [[], []];
-          x.forEach((element) => {
-            z[0].push({ inner: element.innerHTML, id: element.id });
+        function getElementAttrs(el) {
+          var obj = {};
+          var arr = ['disabled', 'class', 'className', 'href', 'src', 'id'];
+          arr.forEach((attr) => {
+            obj[attr] = el[attr];
           });
+          return obj;
+        }
+
+        function diff(arrayOne, arrayTwo) {
+          var arrayThree = [];
+          arrayOne.forEach((element) => {
+            arrayTwo.forEach((element2) => {
+              if (str(element) === str(element2)) {
+                arrayOne = arrayOne.filter(function (item) {
+                  return str(item) !== str(element);
+                });
+                // arrayOne.remove(element);
+              }
+            });
+          });
+          return arrayOne;
+        }
+
+        function childtoarray(x, i = false) {
+          // pass node element , turn it into object of its id and inner html and attrs
+          var z = [[], []];
           // push parent element id , to check if the page has change or not later
-          z[1].push(x[0].parentElement.id);
+          z[1].push(x[0].parentElement.id || false);
+
+          x.forEach((element) => {
+            var indexfromx = [...element.parentElement.children].indexOf(
+              element
+            );
+
+            var index = [...newdom.childNodes].indexOf(element);
+            var oldelement = oldDom.childNodes[index];
+            var value = false;
+            let children = false;
+            if (element.tagName == 'DIV') {
+              children = childtoarray(element.childNodes);
+            }
+            if (oldelement) {
+              value = oldelement;
+            }
+            if (i) {
+              els = i[indexfromx];
+              if (typeof els == 'object') {
+                value = els;
+              }
+            }
+            z[0].push({
+              inner: element.innerHTML || false,
+              id: element.id || value.id,
+              full: element || false,
+              attrs: element.attributes || false,
+              next: element.nextSibling,
+              parent: element.parentElement,
+              previous: element.previousSibling,
+              index: indexfromx,
+              children: children,
+            });
+          });
           return z;
         }
 
-        var old = childtoarray(oldDom.childNodes);
-        var vso = childtoarray(newdom.childNodes);
         var cloneDom = childtoarray(clone.childNodes);
+        var old = childtoarray(oldDom.childNodes);
+        var vso = childtoarray(newdom.childNodes, old[0]);
 
-        // print(old[0].length, vso[0].length);
         // if parent element changed return false
+        if (cloneDom[1][0] !== vso[1][0]) {
+          if (!repeat) {
+            print('not repeat');
+            return tryes(true);
+          } else {
+            print('tryed and false not same', cloneDom[1][0], vso[1][0]);
+            print(newdom);
+            return false;
+          }
+        }
+        // DONT GO NEAR >>>>>>>>>>>>>>>>>>>>>>>>>>DONT GO NEAR
+        var Newi = diff(vso[0], cloneDom[0]);
+        var Oldi = diff(cloneDom[0], vso[0]);
+        // // print('new', Newi, 'old:', Oldi);
+
+        var count = 0;
+        function isInNew(id) {
+          for (const key in Newi) {
+            if (Newi[key].id == id) {
+              return true;
+            }
+          }
+          return false;
+        }
+        function OldiChange(Oldi) {
+          Oldi.forEach((element) => {
+            count = count + 1;
+            if (!isInNew(element.id)) {
+              var ele = el(element.id);
+              if (ele) ele.remove();
+              return;
+            }
+
+            try {
+              var NEWELEMENT = Newi[count];
+              if (NEWELEMENT) {
+                if (NEWELEMENT.inner == false) {
+                  el(NEWELEMENT.id).remove();
+                  return;
+                }
+                var ele = el(NEWELEMENT.id);
+                if (ele) {
+                  if (element.children) {
+                    var oldz = childtoarray(ele.childNodes);
+                    var zero = diff(oldz, NEWELEMENT.children);
+                    var one = diff(NEWELEMENT.children, oldz);
+                    zero.forEach((element) => {
+                      var ele = el(NEWELEMENT.id);
+                      if (ele) ele.replaceWith(NEWELEMENT.full);
+                    });
+                    // return NewiChange(zero);
+                    return;
+                  }
+                  var NewAttrs = getElementAttrs(NEWELEMENT.full);
+                  var OldAttrs = getElementAttrs(element.full);
+                  if (str(NewAttrs) == str(OldAttrs)) {
+                    // print('same');
+                    ele.innerHTML = element.full.innerHTML;
+                    return;
+                  } else {
+                    ele.replaceWith(NEWELEMENT.full);
+                  }
+                } else {
+                  var ele = el(element.id);
+                  if (ele) {
+                    ele.replaceWith(NEWELEMENT.full);
+                    return;
+                  }
+                }
+              }
+            } catch (error) {
+              return;
+            }
+          });
+        }
+        function NewiChange(Newi) {
+          Newi.forEach((element) => {
+            if (element.id) {
+              var ele = el(element.id);
+
+              if (ele) {
+                if (element.children) {
+                  var oldz = childtoarray(ele.childNodes);
+                  var zero = diff(oldz, element.children);
+                  var one = diff(element.children, oldz);
+                  zero.forEach((element) => {
+                    var ele = el(element.id);
+                    if (ele) ele.replaceWith(element.full);
+                  });
+                  // return NewiChange(zero);
+                  return;
+                }
+                var NewAttrs = getElementAttrs(element.full);
+                var OldAttrs = getElementAttrs(ele);
+                if (str(NewAttrs) == str(OldAttrs)) {
+                  // print('same');
+                  ele.innerHTML = element.full.innerHTML;
+                  return;
+                } else {
+                  print('changed', element.id);
+                  ele.replaceWith(element.full);
+                }
+              }
+              // print(element);
+              var parent = element.parent;
+              var index = element.index;
+              var realParent = el(parent.id);
+              if (realParent) {
+                realParent.insertChildAtIndex(element.full, index);
+              }
+            } else {
+            }
+          });
+        }
+
+        window.fullscreen = document.fullscreenElement;
+
+        OldiChange(Oldi);
+        NewiChange(Newi);
+        if (fullscreen) {
+          el(fullscreen.id).requestFullscreen();
+        }
+
+        // DONT GO NEAR<<<<<<<<<<<<<<<<<<<<<<<<<<DONT GO NEAR
+        // oldDom = root.childNodes[0];
+        // if (oldDom.innerHTML == newdom.innerHTML) {
+        //   // print('YAAAAY');
+        //   window.finished = true;
+        //   window.succeed = window.succeed + 1;
+        //   return true;
+        // } else {
+        //   // print('NOOO');
+        //   print('New:', Newi, 'Old:', Oldi);
+        //   window.finished = true;
+        //   window.fails = window.fails + 1;
+        //   return false;
+        // }
+        // print('New:', Newi, 'Old:', Oldi);
+
+        return true;
+
+        if (!repeat) {
+          return tryes(true);
+        }
+        window.finished = true;
+
+        // print(diss, cloneDom[0]);
+        // breakS
 
         if (cloneDom[1][0] !== vso[1][0]) {
-          // print(cloneDom[1][0], vso[1][0]);
           if (!repeat) {
-            if (external) newdom = external;
-            else newdom = window.vdom();
-            return tryes(true);
+            if (!external) {
+              newdom = window.vdom();
+              return tryes(true);
+            } else return false;
           } else {
             print('tryed and false not same', cloneDom[1][0], vso[1][0]);
             return false;
           }
-        } else {
-          return false;
         }
         // print(false);
-
         try {
           vso[0].forEach((element) => {
             var index = vso[0].indexOf(element);
@@ -189,6 +415,12 @@ function UPDATE_DOM(newdom = false) {
               var ele = clone.querySelector('#' + olds.id);
               if (ele) {
                 ele.innerHTML = element.inner;
+              } else {
+                var id = olds.id;
+                var ele = el(id);
+                if (ele) {
+                  ele.remove();
+                }
               }
             }
           });
@@ -200,6 +432,12 @@ function UPDATE_DOM(newdom = false) {
                 var ele = el(olds.id);
                 if (ele) {
                   ele.innerHTML = element.inner;
+                } else {
+                  var id = olds.id;
+                  var ele = el(id);
+                  if (ele) {
+                    ele.remove();
+                  }
                 }
               }
             });
@@ -210,6 +448,7 @@ function UPDATE_DOM(newdom = false) {
                 return tryes(true);
               } else return false;
             } else {
+              print('tryed and false not same', cloneDom[1][0], vso[1][0]);
               return false;
             }
           }
@@ -221,7 +460,7 @@ function UPDATE_DOM(newdom = false) {
 
         return true;
       } catch (error) {
-        // print(error.message);
+        print(error);
         console.log('error in tryes() in updateDom');
         return false;
       }
@@ -231,12 +470,11 @@ function UPDATE_DOM(newdom = false) {
 
     if (tryes() == false) {
       window.finished = true;
-      // print('returned false');
+      print('returned false');
       var div = document.createElement('div');
       div.id = 'parent';
-      div.append(newdom);
+      div.append(ThisPage()());
       root.replaceWith(div);
-
       if (window.focused) {
         var focus = el(window.focused);
         if (focus) {
@@ -250,7 +488,7 @@ function UPDATE_DOM(newdom = false) {
       print('found on update');
       window.onUpdate();
     }
-    window.els = {};
+    els = {};
     window.finished = true;
 
     return;
@@ -302,8 +540,15 @@ window.ThisPage = () => {
 
 // MAIN THING , RENDERS ON DOM COMPLETE >> Sends passed function to INIT()
 function render(x) {
-  APP.Modules['Main'] = x;
-  APP.Routes['Main'] = x;
+  function Mok() {
+    var React = React;
+    var SET_VALUES = SET_VALUES;
+    els = {};
+    return x();
+  }
+  APP.Modules['Main'] = Mok;
+  APP.Routes['Main'] = Mok;
+
   window.vdom = x;
   let stateCheck = setInterval(() => {
     if (document.readyState === 'complete') {
@@ -354,11 +599,17 @@ async function SetModulesFile(path, names) {
 // }
 
 // Set modules takes the function name and assigns it to APP object on window and Modules Object
+
 var Modules = {
   SetModule: function (z, x) {
     var name = z.name;
-    this[name] = z;
-    window.APP[name] = z;
+    window.APP[name] = (x) => {
+      var React = React;
+      var SET_VALUES = SET_VALUES;
+      els = {};
+      return z(x);
+    };
+    this[name] = window.APP[name];
   },
   SetModulesFile: SetModulesFile,
 };
@@ -379,10 +630,14 @@ function Router(z) {
 }
 
 // SET PAGE FOR HASH AND HASH FOR MODULE
+
 var Route = (x, z) => {
   APP.Modules[x] = z;
   if (typeof z == 'function') {
     function Callee() {
+      var React = React;
+      var SET_VALUES = SET_VALUES;
+      els = {};
       location.hash = x;
       return z();
     }
@@ -390,7 +645,11 @@ var Route = (x, z) => {
     window.APP.Routes[x] = Callee;
   } else {
     function Callee() {
+      var React = React;
+      var SET_VALUES = SET_VALUES;
       location.hash = x;
+      els = {};
+
       return z;
     }
 
@@ -399,7 +658,6 @@ var Route = (x, z) => {
 };
 
 window.updates = {};
-
 // i really dont know dude I THOUGHT IT'S COOL ¯\(o_o)/¯
 
 function useEffect(fn, mainfn) {
@@ -426,11 +684,11 @@ function useState(initialValue, Callback = false) {
   };
 
   function setState(newVal, Callbacks = false) {
-    window.els = {};
+    els = {};
     if (!window.Loaded) {
-      window.Loaded = true;
       window.finished = true;
     }
+
     try {
       clearTimeout(window.timeout);
       // print('cleared');
@@ -523,7 +781,12 @@ window.onhashchange = (e) => {
   if (window.APP.Routes[name] !== undefined) {
     return UPDATE_DOM(window.APP.Routes[name]);
   } else {
-    location.hash = '';
-    UPDATE_DOM();
+    // location.hash = '';
+    // UPDATE_DOM();
   }
 };
+
+import * as NaminAnythingBlah from './babel.min.js';
+
+window.fails = 0;
+window.succeed = 0;
